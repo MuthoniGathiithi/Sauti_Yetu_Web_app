105e91f7a4301e8d85fa2c512c8192b1894b7bce
Joyce <gathiithijoyce74@gmail.com>
2025-07-09 18:09:32 +0300

    updated the sauti_backend urls.py

--- sauti_backend/urls.py
+++ sauti_backend/urls.py
@@ -23,9 +23,10 @@ from users.views import landing_page
 urlpatterns = [
     path('', landing_page, name='home'),  # Make landing page the root URL
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
     path('reports/', include('reports.urls')),  # Add this line
     path('resources/', include(('resources.urls', 'resources'), namespace='resources')),
+
 
  
 ]
